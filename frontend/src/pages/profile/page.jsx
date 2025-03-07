676d76fa537823b8c9f8b88db6a809056e8c6066
WesleyWBFranco <wesleyfranco@alunos.utfpr.edu.br>
2025-03-07 06:22:10 -0300

    Recebendo os dados da API REST, fazendo tela profile do usuário com os seus dados e os pedidos

--- frontend/src/pages/profile/page.jsx
+++ frontend/src/pages/profile/page.jsx
@@ -1,28 +1,60 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import authServices from "../../services/auth"
+import orderServices from "../../services/order"
+import styles from './page.module.css'
 
 export default function Profile() {
     const { logout } = authServices()
+    const { getUserOrders, orderLoading, refetchOrders, ordersList } = orderServices()
     const navigate = useNavigate()
     const authData = JSON.parse(localStorage.getItem('auth'))
 
     useEffect(() => {
         if(!authData){
             navigate('/auth')
+        } else if(refetchOrders) {
+            getUserOrders(authData?.user?._id)
         }
-    }, [authData, navigate])
+    }, [authData, navigate, refetchOrders])
+
+    if(orderLoading) {
+        return( <h1>Loading...</h1> )
+    }
 
     const handleLogout = () => {
         logout()
         return navigate('/')
     } 
 
+    console.log(ordersList)
+
     return (
-        <>
+        <div className={styles.pageContainer}>
             <h1>{authData?.user?.fullname}</h1>
             <h3>{authData?.user?.email}</h3>
             <button onClick={handleLogout}>Logout</button>
-        </>
+
+            {ordersList.length > 0 ?
+                <div className={styles.ordersContainer}>
+                    {ordersList.map((order) => (
+                        <div key={order._id} className={styles.orderContainer}>
+                            <h3>{order.pickupTime}</h3>
+                            <p>{order.pickupStatus}</p>
+                            {order.orderItems.map((item) => (
+                                <div key={item._id}>
+                                    <h4>{item.itemDetails[0].name}</h4>
+                                    <p>Quantity: {item.quantity }</p>
+                                </div>    
+                            ))}
+                        </div>               
+                    ))}
+                </div>
+            :
+                <div>   
+                    You do not have orders yet
+                </div>
+            }
+        </div>
     )
 }
